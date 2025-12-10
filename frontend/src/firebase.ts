53411b47bbb8dbbfe092845617077ba7a343dcd6
Jacob Freise <jacob.freise@fu-berlin.de>
2025-12-10 18:58:37 +0100

    firebase emulators + env file config

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -1,18 +1,37 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+
+// const firebaseConfig = {
+//     apiKey: "REDACTED_FIREBASE_API_KEY",
+//     authDomain: "raggooneropen.firebaseapp.com",
+//     databaseURL: "https://raggooneropen-default-rtdb.europe-west1.firebasedatabase.app",
+//     projectId: "raggooneropen",
+//     storageBucket: "raggooneropen.firebasestorage.app",
+//     messagingSenderId: "389145362446",
+//     appId: "1:389145362446:web:907a5c2f2c30a11db97c5f",
+//     measurementId: "G-QWVW75P7MC"
+// };
 
 const firebaseConfig = {
-    apiKey: "REDACTED_FIREBASE_API_KEY",
-    authDomain: "raggooneropen.firebaseapp.com",
-    databaseURL: "https://raggooneropen-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "raggooneropen",
-    storageBucket: "raggooneropen.firebasestorage.app",
-    messagingSenderId: "389145362446",
-    appId: "1:389145362446:web:907a5c2f2c30a11db97c5f",
-    measurementId: "G-QWVW75P7MC"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+if (location.hostname === "localhost") {
+    console.log("🔧 connecting to emulators...");
+    connectFirestoreEmulator(db, '127.0.0.1', 8080);
+    connectAuthEmulator(auth, 'http://127.0.0.1:9099');
+}
+
+export { db, auth };
