c6d5d1c61f0287cb373e3affaaa0c7a2df8f3633
Jacob Freise <jacob.freise@fu-berlin.de>
2026-02-24 18:28:00 +0100

    format description

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -27,7 +27,7 @@ export const TOURNAMENT_FORMATS: Record<string, TournamentFormat> = {
         name:
             'Ban Umas',
         description:
-            'Classic Tournament format with 1 Uma ban per team.'
+            'Classic Tournament format with 1 Uma ban per team and Uma blind picks.'
     },
     'uma-draft': {
         id: 'uma-draft',
